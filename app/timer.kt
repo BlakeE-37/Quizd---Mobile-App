be9dbfec60976f3224c3b7369fa3df97e0cea3ac
gib-student <gib19021@byui.edu>
2022-02-08 17:40:18 -0700

    Started building timer in Kotlin

--- app/timer.kt
+++ app/timer.kt
@@ -1,3 +1,9 @@
 fun CountDown() : Int {
-
+//    time starts at 4 seconds so that it decrements once then proceeds
+    while (/*3 secodns has not passed */){
+//        decrement time by 1 second
+        while (/*second has not passed */) {
+//            continue to update time
+        }
+    }
 }
