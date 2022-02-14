6c5fcc613f384518303ea14b855a1a734851140d
Blake Hollingsworth <Blake.Hollingsworth80@gmail.com>
2022-02-14 15:18:03 -0700

    Screen to screen transitions

---
+++ app/src/main/java/com/example/helloworld/playScreen.kt
@@ -0,0 +1,22 @@
+package com.example.helloworld
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class PlayScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_screen)
+
+        val playButton: Button = findViewById(R.id.button5)
+        playButton.setOnClickListener(){ categoryScreen() }
+
+    }
+    private fun categoryScreen(){
+        val startActivity = Intent(this@PlayScreen, CategoryActivity::class.java)
+        startActivity(startActivity)
+    }
+}
